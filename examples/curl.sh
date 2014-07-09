f0bb51cc51d67f88ce1b8746d013df7c4541a39c
Keith Pitt <me@keithpitt.com>
2014-07-09 14:28:50 +0800

    Added a file to generate raw and rendered output from a command.

---
+++ examples/curl.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo -e "\033[90m$\033[0m curl -o /tmp/file.txt https://example.com/file.txt"
+curl -o /tmp/buildbox-agent.tar.gz https://github.com/buildboxhq/buildbox-agent/releases/download/v0.1-alpha/buildbox-agent-darwin-386.tar.gz
