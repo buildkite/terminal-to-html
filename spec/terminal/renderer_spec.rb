7ba70847d0ca8f1b9312069b8783bf2ed88ec952
Keith Pitt <me@keithpitt.com>
2014-07-09 14:44:11 +0800

    Added some failing tests.

--- spec/terminal/renderer_spec.rb
+++ spec/terminal/renderer_spec.rb
@@ -10,4 +10,32 @@ describe Terminal::Renderer do
       expect(renderer.render(fixture.raw)).to eql(fixture.rendered)
     end
   end
+
+  describe "#render" do
+    let(:hello_output) { "he\e[81mllo" }
+
+    it "closes colors that get opened" do
+      raw = "he\e[81mllo"
+
+      expect(renderer.render(raw)).to eql("he<span class='c81'>llo</span>")
+    end
+
+    it "backspaces colors" do
+      raw = "he\e[81m\e[90m\bllo"
+
+      expect(renderer.render(raw)).to eql("hello")
+    end
+
+    it "starts overwriting characters when you \\r midway through somehing" do
+      raw = "hello\rb"
+
+      expect(renderer.render(raw)).to eql("bello")
+    end
+
+    it "colors across multiple lines" do
+      raw = "\e[81mhello\n\nfriend\e[0m"
+
+      expect(renderer.render(raw)).to eql("<span class='c81'>hello\n&nbsp;\nfriend</span>")
+    end
+  end
 end
