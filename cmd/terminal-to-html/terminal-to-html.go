90131ebf9898d4191cc2e72695d8d10137d806c4
Michael Pearson <mipearson@gmail.com>
2015-03-01 15:07:16 +1100

    Begin tidying up help output with use of codegangsta/cli

--- cmd/terminal-to-html/terminal-to-html.go
+++ cmd/terminal-to-html/terminal-to-html.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -10,6 +9,7 @@ import (
 	"os"
 
 	"github.com/buildkite/terminal"
+	"github.com/codegangsta/cli"
 )
 
 func check(m string, e error) {
@@ -18,34 +18,48 @@ func check(m string, e error) {
 	}
 }
 
-var serve = flag.String("http", "", "HTTP service address (e.g., ':6060')")
+func webservice(listen string) {
+	http.HandleFunc("/terminal", func(w http.ResponseWriter, r *http.Request) {
+		input, err := ioutil.ReadAll(r.Body)
+		check("could not read from HTTP stream", err)
+		w.Write(terminal.Render(input))
+	})
+
+	log.Printf("Listening on %s", listen)
+	log.Fatal(http.ListenAndServe(listen, nil))
+}
+
+func stdin() {
+	var input []byte
+	var err error
+	if len(flag.Arg(0)) > 0 {
+		input, err = ioutil.ReadFile(flag.Arg(0))
+		check(fmt.Sprintf("could not read %s", flag.Arg(0)), err)
+	} else {
+		input, err = ioutil.ReadAll(os.Stdin)
+		check("could not read stdin", err)
+	}
+	fmt.Printf("%s", terminal.Render(input))
+}
 
 func main() {
-	flag.ErrHelp = errors.New("flag: help requested")
-
-	flag.Parse()
-
-	if *serve != "" {
-		http.HandleFunc("/terminal", func(w http.ResponseWriter, r *http.Request) {
-			input, err := ioutil.ReadAll(r.Body)
-
-			check("could not read from HTTP stream", err)
-			output := terminal.Render(input)
-			w.Write(output)
-		})
-
-		log.Printf("Listening on %s", *serve)
-		log.Fatal(http.ListenAndServe(*serve, nil))
-	} else {
-		var input []byte
-		var err error
-		if len(flag.Arg(0)) > 0 {
-			input, err = ioutil.ReadFile(flag.Arg(0))
-			check(fmt.Sprintf("could not read %s", flag.Arg(0)), err)
-		} else {
-			input, err = ioutil.ReadAll(os.Stdin)
-			check("could not read stdin", err)
-		}
-		fmt.Printf("%s", terminal.Render(input))
+	app := cli.NewApp()
+	app.Name = "terminal-to-html"
+	app.Usage = "input ANSI on STDIN, output HTML to STDOUT"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "http",
+			Value: "",
+			Usage: "HTTP service mode (eg --http :6060), endpoint is /terminal",
+		},
 	}
+	app.Action = func(c *cli.Context) {
+		if c.String("http") != "" {
+			webservice(c.String("http"))
+		} else {
+			stdin()
+		}
+	}
+	app.Run(os.Args)
+
 }
