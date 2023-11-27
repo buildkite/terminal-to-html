7a356298ca7caf24f09a1c8fc71d6762718b49bf
Narthana Epa <narthana.epa@gmail.com>
2023-11-27 11:43:47 +1100

    Delete dependencies

--- cmd/terminal-to-html/terminal-to-html.go
+++ cmd/terminal-to-html/terminal-to-html.go
@@ -1,93 +1,19 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
 	"io"
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/buildkite/terminal-to-html/v3"
-	"github.com/buildkite/terminal-to-html/v3/internal/assets"
-	"github.com/urfave/cli/v2"
 )
-
-var AppHelpTemplate = `{{.Name}} - {{.Usage}}
-
-STDIN/STDOUT USAGE:
-  cat input.raw | {{.Name}} [arguments...] > out.html
-
-WEBSERVICE USAGE:
-  {{.Name}} --http :6060 &
-  curl --data-binary "@input.raw" http://localhost:6060/terminal > out.html
-
-OPTIONS:
-  {{range .Flags}}{{.}}
-  {{end}}
-`
-
-var PreviewMode = false
-
-var PreviewTemplate = `
-	<!DOCTYPE html>
-	<html>
-		<head>
-			<meta charset="UTF-8">
-			<title>terminal-to-html Preview</title>
-			<style>STYLESHEET</style>
-		</head>
-		<body>
-			<div class="term-container">CONTENT</div>
-		</body>
-	</html>
-`
 
 func check(m string, e error) {
 	if e != nil {
 		log.Fatalf("%s: %v", m, e)
 	}
-}
-
-func wrapPreview(s []byte) ([]byte, error) {
-	if PreviewMode {
-		s = bytes.Replace([]byte(PreviewTemplate), []byte("CONTENT"), s, 1)
-		styleSheet, err := assets.TerminalCSS()
-		if err != nil {
-			return nil, err
-		}
-		s = bytes.Replace(s, []byte("STYLESHEET"), styleSheet, 1)
-	}
-	return s, nil
-}
-
-func webservice(listen string) {
-	http.HandleFunc("/terminal", func(w http.ResponseWriter, r *http.Request) {
-		input, err := io.ReadAll(r.Body)
-		if err != nil {
-			log.Printf("could not read from HTTP stream: %v", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprint(w, "Error reading request.")
-			return
-		}
-
-		respBody, err := wrapPreview(terminal.Render(input))
-		if err != nil {
-			log.Printf("error wrapping preview: %v", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprint(w, "Error creating preview.")
-			return
-		}
-
-		_, err = w.Write(respBody)
-		if err != nil {
-			log.Printf("error writing response: %v", err)
-		}
-	})
-
-	log.Printf("Listening on %s", listen)
-	log.Fatal(http.ListenAndServe(listen, nil))
 }
 
 func stdin() {
@@ -100,41 +26,10 @@ func stdin() {
 		input, err = io.ReadAll(os.Stdin)
 		check("could not read stdin", err)
 	}
-	output, err := wrapPreview(terminal.Render(input))
-	check("could not wrap preview", err)
-	fmt.Printf("%s", output)
+	output := terminal.Render(input)
+	fmt.Printf("%s\n", output)
 }
 
 func main() {
-	cli.AppHelpTemplate = AppHelpTemplate
-
-	app := cli.NewApp()
-
-	app.Name = "terminal-to-html"
-	app.Version = terminal.Version()
-	app.Usage = "turn ANSI in to HTML"
-	app.Flags = []cli.Flag{
-		&cli.StringFlag{
-			Name:  "http",
-			Value: "",
-			Usage: "HTTP service mode (eg --http :6060), endpoint is /terminal",
-		},
-		&cli.BoolFlag{
-			Name:  "preview",
-			Usage: "wrap output in HTML & CSS so it can be easily viewed directly in a browser",
-		},
-	}
-	app.Action = func(c *cli.Context) error {
-		PreviewMode = c.Bool("preview")
-		if c.String("http") != "" {
-			webservice(c.String("http"))
-		} else {
-			stdin()
-		}
-		return nil
-	}
-
-	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
-	}
+	stdin()
 }
--- go.mod
+++ go.mod
@@ -2,13 +2,4 @@ module github.com/buildkite/terminal-to-html/v3
 
 go 1.20
 
-require (
-	github.com/google/go-cmp v0.5.9
-	github.com/urfave/cli/v2 v2.25.7
-)
-
-require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
-)
+require github.com/google/go-cmp v0.5.9
