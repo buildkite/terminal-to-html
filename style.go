1eca147aa1ca2f8f99aadfc2d89813cccd018437
Zoey Greer <zoey@buildbuddy.io>
2025-03-25 16:33:19 -0400

    handle parsing 24-bit colors

--- style.go
+++ style.go
@@ -5,17 +5,15 @@ import "strconv"
 type style uint64
 
 // style encoding:
-// 0... ...23  24... ...47  48...59     60     61   62....63
+// 0... ...23  24... ...47  48...57     58     59   60....63
 // [fg color]  [bg color]   [flags]  element  link  [unused]
 // flags = bold, faint, etc
 
 const (
 	sbFGColorX1 = style(1) << (48 + iota)
 	sbFGColorX2
-	sbFGColorX3
 	sbBGColorX1
 	sbBGColorX2
-	sbBGColorX3
 	sbBold
 	sbFaint
 	sbItalic
@@ -27,13 +25,12 @@ const (
 )
 
 const (
-	sbFGColorX = sbFGColorX1 | sbFGColorX2 | sbFGColorX3
-	sbBGColorX = sbBGColorX1 | sbBGColorX2 | sbBGColorX3
+	sbFGColorX = sbFGColorX1 | sbFGColorX2
+	sbBGColorX = sbBGColorX1 | sbBGColorX2
 )
 
 const (
 	colorNone = uint8(iota)
-	colorDefault
 	colorSGR
 	color8Bit
 	color24Bit
@@ -41,7 +38,7 @@ const (
 
 
 // Used for comparing styles - ignores the element bit, link bit, and unused bits.
-const styleComparisonMask = 0x0fff_ffff_ffff_ffff
+const styleComparisonMask = 0x03ff_ffff_ffff_ffff
 
 // isPlain reports if there is no style information. elements (that have no
 // other style set) are also considered plain.
@@ -50,7 +47,7 @@ func (s style) isPlain() bool { return s&styleComparisonMask == 0 }
 func (s style) fgColor() uint32  { return uint32(s & 0x0000_00ff_ffff) }
 func (s style) fgColorType() uint8  { return uint8((s&sbFGColorX) >> 48) }
 func (s style) bgColor() uint32  { return uint32((s & 0xffff_ff00_0000) >> 24) }
-func (s style) bgColorType() uint8  { return uint8((s&sbBGColorX) >> 51) }
+func (s style) bgColorType() uint8  { return uint8((s&sbBGColorX) >> 50) }
 func (s style) bold() bool       { return s&sbBold != 0 }
 func (s style) faint() bool      { return s&sbFaint != 0 }
 func (s style) italic() bool     { return s&sbItalic != 0 }
@@ -60,15 +57,15 @@ func (s style) blink() bool      { return s&sbBlink != 0 }
 func (s style) element() bool    { return s&sbElement != 0 }
 func (s style) hyperlink() bool  { return s&sbHyperlink != 0 }
 
-func (s *style) setFGColorDefault()  { *s = (*s &^ 0x7_0000_00ff_ffff) | (style(colorDefault) << 48) }
-func (s *style) setFGColorSGR(v uint8)  { *s = (*s &^ 0x7_0000_00ff_ffff) | style(v) | (style(colorSGR) << 48) }
-func (s *style) setFGColor8Bit(v uint8)  { *s = (*s &^ 0x7_0000_00ff_ffff) | style(v) | (style(color8Bit) << 48) }
-func (s *style) setFGColor24Bit(rgb [3]uint8)  { *s = (*s &^ 0x7_0000_00ff_ffff) | (style(rgb[0]) << 16) | (style(rgb[1]) << 8) | style(rgb[2]) | (style(color24Bit) << 48) }
+func (s *style) resetFGColor()  { *s = (*s &^ 0x3_0000_00ff_ffff) }
+func (s *style) setFGColorSGR(v uint8)  { *s = (*s &^ 0x3_0000_00ff_ffff) | style(v) | (style(colorSGR) << 48) }
+func (s *style) setFGColor8Bit(v uint8)  { *s = (*s &^ 0x3_0000_00ff_ffff) | style(v) | (style(color8Bit) << 48) }
+func (s *style) setFGColor24Bit(rgb [3]uint8)  { *s = (*s &^ 0x3_0000_00ff_ffff) | (style(rgb[0]) << 16) | (style(rgb[1]) << 8) | style(rgb[2]) | (style(color24Bit) << 48) }
 
-func (s *style) setBGColorDefault()  { *s = (*s &^ 0x38_ffff_ff00_0000) | (style(colorDefault) << 51) }
-func (s *style) setBGColorSGR(v uint8)  { *s = (*s &^ 0x38_ffff_ff00_0000) | (style(v) << 24) | (style(colorSGR) << 51) }
-func (s *style) setBGColor8Bit(v uint8)  { *s = (*s &^ 0x38_ffff_ff00_0000) | (style(v) << 24) | (style(color8Bit) << 51) }
-func (s *style) setBGColor24Bit(rgb [3]uint8)  { *s = (*s &^ 0x38_ffff_ff00_0000) | (style(rgb[0]) << 40) | (style(rgb[1]) << 32) | (style(rgb[2]) << 24) | (style(color24Bit) << 51) }
+func (s *style) resetBGColor()  { *s = (*s &^ 0xc_ffff_ff00_0000) }
+func (s *style) setBGColorSGR(v uint8)  { *s = (*s &^ 0xc_ffff_ff00_0000) | (style(v) << 24) | (style(colorSGR) << 50) }
+func (s *style) setBGColor8Bit(v uint8)  { *s = (*s &^ 0xc_ffff_ff00_0000) | (style(v) << 24) | (style(color8Bit) << 50) }
+func (s *style) setBGColor24Bit(rgb [3]uint8)  { *s = (*s &^ 0xc_ffff_ff00_0000) | (style(rgb[0]) << 40) | (style(rgb[1]) << 32) | (style(rgb[2]) << 24) | (style(color24Bit) << 50) }
 
 func (s *style) setBold(v bool)      { *s = (*s &^ sbBold) | booln(v, sbBold) }
 func (s *style) setFaint(v bool)     { *s = (*s &^ sbFaint) | booln(v, sbFaint) }
@@ -94,9 +91,6 @@ func (s style) asClasses() []string {
 	var styles []string
 
 	switch s.fgColorType() {
-	case colorDefault:
-		// This should reset the color to default, but the previous behavior was to ignore it,
-		// so we'll do that.
 	case colorSGR:
 		if s.fgColor() > 29 && s.fgColor() < 38 {
 			styles = append(styles, "term-fg"+strconv.Itoa(int(s.fgColor())))
@@ -112,9 +106,6 @@ func (s style) asClasses() []string {
 	}
 
 	switch s.bgColorType() {
-	case colorDefault:
-		// This should reset the color to default, but the previous behavior was to ignore it,
-		// so we'll do that.
 	case colorSGR:
 		if s.bgColor() > 39 && s.bgColor() < 48 {
 			styles = append(styles, "term-bg"+strconv.Itoa(int(s.bgColor())))
@@ -186,9 +177,9 @@ func (s style) color(colors []string) style {
 		case COLOR_GOT_48:
 			switch cc {
 			case 5:
-				colorMode = COLOR_GOT_38_5
+				colorMode = COLOR_GOT_48_5
 			case 2:
-				colorMode = COLOR_GOT_38_2
+				colorMode = COLOR_GOT_48_2
 				rgb_index = 0
 			default:
 				colorMode = COLOR_NORMAL
@@ -254,11 +245,11 @@ func (s style) color(colors []string) style {
 		case 38:
 			colorMode = COLOR_GOT_38
 		case 39:
-			s.setFGColorDefault()
+			s.resetFGColor()
 		case 48:
 			colorMode = COLOR_GOT_48
 		case 49:
-			s.setBGColorDefault()
+			s.resetBGColor()
 		case 30, 31, 32, 33, 34, 35, 36, 37, 90, 91, 92, 93, 94, 95, 96, 97:
 			s.setFGColorSGR(uint8(cc))
 		case 40, 41, 42, 43, 44, 45, 46, 47, 100, 101, 102, 103, 104, 105, 106, 107:
