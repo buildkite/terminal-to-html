3cb6187d89723c4db543b66f19400b6e859f6461
Zoey Greer <tempozrene@gmail.com>
2025-04-09 10:09:37 -0400

    Fix `screen_test` to use `t.Run` and use `Fatalf` when `NewScreen` fails
    Co-authored-by: Josh Deprez <josh.deprez@gmail.com>

--- screen_test.go
+++ screen_test.go
@@ -34,22 +34,20 @@ var currentLineForWritingTestCases = []struct {
 
 func TestCurrentLineForWriting(t *testing.T) {
 	for _, test := range currentLineForWritingTestCases {
-		s, err := NewScreen(WithMaxSize(0, test.maxlines))
-		got := []string{}
-		s.ScrollOutFunc = func(line string) { got = append(got, line) }
-		_ = s.currentLineForWriting()
-		s.Write([]byte(test.input))
-		if err != nil {
-			t.Errorf("Failure for '%s':\nNewScreen returned an error: %s", test.name, err)
-		}
-		_ = s.currentLineForWriting()
+		t.Run(test.name, func(t *testing.T) {
+			s, err := NewScreen(WithMaxSize(0, test.maxlines))
+			if err != nil {
+				t.Fatalf("NewScreen(WithMaxSize(0, %d)) error: %s", test.maxlines, err)
+			}
+			got := []string{}
+			s.ScrollOutFunc = func(line string) { got = append(got, line) }
+			_ = s.currentLineForWriting()
+			s.Write([]byte(test.input))
+			_ = s.currentLineForWriting()
 
-		if diff := cmp.Diff(got, test.want); diff != "" {
-			t.Errorf(
-				"Failure for '%s':\nscrolledOutFunc sequence of parameters diff (-got +want):\n%s",
-				test.name,
-				diff,
-			)
-		}
+			if diff := cmp.Diff(got, test.want); diff != "" {
+				t.Errorf("scrolledOutFunc sequence of parameters diff (-got +want):\n%s", diff)
+			}
+		})
 	}
 }
